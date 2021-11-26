606ff02f004e1af0687de1845861b0fb4769996e
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-26 06:50:13 +0600

    Update job.sql

--- INSTALL ME FIRST/sql for database/job.sql
+++ INSTALL ME FIRST/sql for database/job.sql
@@ -1,11 +1,14 @@
+DELETE FROM `addon_account` WHERE `name` = 'society_hookahlounge';
 INSERT INTO `addon_account` (name, label, shared) VALUES
 	('society_hookahlounge', 'Hookah Lounge', 1)
 ;
 
+DELETE FROM `jobs` WHERE `name` = 'hookahlounge';
 INSERT INTO `jobs` (name, label) VALUES
 	('hookahlounge', 'Hookah Lounge')
 ;
 
+DELETE FROM `job_grades` WHERE `job_name` = 'hookahlounge';
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
 	('hookahlounge',0,'employee','Employee',20,'{}','{}'),
 	('hookahlounge',1,'shopkeeper','Shopkeeper',40,'{}','{}'),
